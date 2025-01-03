1017557bba0d6ffa44633b53eaf2f7bf1cf89ef9
Abdirizak Mohamed <inzaghi3650@gmail.com>
2025-01-03 16:36:14 -0600

    Added more comments

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // StrictMode is a tool for highlighting potential problems in an application 
   <StrictMode>
     <App />
   </StrictMode>,
